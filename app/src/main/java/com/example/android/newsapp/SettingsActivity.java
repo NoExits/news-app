4b32f57e759d65450bb6c19c9d8e6aa2430a7562
Roland <roland.sarosy@gmail.com>
2018-06-09 13:38:09 +0200

    added onPreferenceChangeListener(s)

--- app/src/main/java/com/example/android/newsapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/newsapp/SettingsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.newsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -11,11 +14,29 @@ public class SettingsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
-    public static class NewsPreferenceFragment extends android.preference.PreferenceFragment {
+    public static class NewsPreferenceFragment extends android.preference.PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState){
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            Preference searchTerm = findPreference(getString(R.string.settings_search_term_key));
+            bindPreferenceSummaryToValue(searchTerm);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            //Take care of updating the displayed preference summary after it has been updated
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        private void bindPreferenceSummaryToValue(Preference preference) {
+            preference.setOnPreferenceChangeListener(this);
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
         }
     }
 }
